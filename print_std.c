8e171b9a1fa3d55a51028a5e87ec8b60af588eef
Yassire Daniel Allaoui <166159376+Ddyas13@users.noreply.github.com>
2026-09-06 18:11:33 +0200

    Update print_std.c
    Coo

--- print_std.c
+++ print_std.c
@@ -1,106 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_std.c                                        :+:      :+:    :+:   */
+/*   valid_print.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dyassire <dyassire@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/06 10:59:27 by dyassire          #+#    #+#             */
-/*   Updated: 2026/09/06 17:45:18 by dyassire         ###   ########.fr       */
+/*   Created: 2026/09/06 15:05:25 by dyassire          #+#    #+#             */
+/*   Updated: 2026/09/06 17:46:01 by dyassire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include "header.h"
 
-/*++++++++++++++++++++++++++++++++++++++++++++++*/
-/* ce bloc est consacre au fonction d'affichage de base */
-void	ft_putchar(char c)
+int	valid_print(int l, int c, int grille[4][4])
 {
-	write(1, &c, 1);
-}
+	int	i;
 
-void	ft_putnbr(int nb)
-{
-	if (nb > 10)
+	i = 0;
+	while (i < 4)
 	{
-		ft_putnbr(nb /10);
+		if (i != c && grille[l][i] == grille[l][c] && grille[l][c] != 0)
+		{
+			return (0);
+		}
+		i++;
 	}
-	else
+	i = 0;
+	while (i < 4)
 	{
-		ft_putchar((nb % 10) + '0');
+		if (i != l && grille[i][c] == grille[l][c] && grille[l][c] != 0)
+		{
+			return (0);
+		}
+		i++;
 	}
-}
-
-void	ft_putstr(char *str)
-{
-	int	count;
-
-	count = 0;
-	while (!(*str == '\0'))
-	{
-		ft_putchar(*str);
-		str++;
-		count++;
-	}
-}
-/*++++++++++++++++++++++++++++++++++++++++++++++*/
-/*ce bloc affiche les bordures et le tableau en dur */
-int	ft_print_border(void)
-{
-	int c;
-
-	ft_putstr(" +");
-	c = 0;
-	while (c < 4)
-	{
-		ft_putstr("---+");
-		c++;
-	}
-	ft_putchar('\n');
-}
-
-// affciher les bordure et les indices en formes 
-void	ft_print_gird(int grille[4][4], int valeurs[16])
-{
-	int	l;
-	int	c;
-
-	// pour les indices du haut 
-	ft_putstr(" ");
-	c = 0;
-	while (c < 4)
-	{
-		ft_putchar(" ");
-		ft_putnbr(valeurs[c]);
-		ft_putstr(" ");
-		c++;
-	}
-	ft_putchar('\n');
-	ft_print_border();
-	// pour la ligne avec les indices gauche et a droite 
-	l = 0;
-	while (1 < 4)
-	{
-		ft_punbr(valeurs[8 +1]);
-		ft_putchar(" ");
-		ft_putchar('|');
-		c++;
-	}
-	ft_putchar(' ' );
-	ft_putnbr(valeurs[12 + 1]);
-	ft_putchar('\n');
-	ft_print_border();
-	i++;
-
-	// pour les indices du bas
-	 ft_pustr(" ");
-	 c = 0;
-	 while (c < 4)
-	 {
-		 ft_putchar(' ');
-		 ft_putnbr(valeurs[4 + c]);
-		 ft_putstr(" ");
-		 c++;
-	 }
+	return (1);
 }
